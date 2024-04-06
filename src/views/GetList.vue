7f2e51988a28aecd9875a5288f77dc7a968ce0ed
Carlos Vélez <80561943+carlsevels@users.noreply.github.com>
2024-04-06 10:53:08 -0600

    gragica load

--- src/views/GetList.vue
+++ src/views/GetList.vue
@@ -1,45 +1,134 @@
+<style>
+/* Estilos para la impresión */
+@media print {
+
+  /* Estilos para la impresión */
+  .logo-print {
+    display: none;
+    /* Mostrar el logo solo durante la impresión */
+  }
+
+  .text-print {
+    display: none;
+    /* Mostrar el texto solo durante la impresión */
+  }
+}
+
+/* Estilos para la vista normal */
+.logo-print {
+  display: block;
+}
+
+.text-print {
+  display: block;
+  /* Ocultar el logo y el texto en la vista normal */
+}
+</style>
+
 <template>
-    <div>
-        <v-row>
-            <v-col>
-                <h1>Users</h1>
-            </v-col>
-            <v-co>
-                <v-btn @click="convertToPDF" variant="tonal">
-                    Imprimir
-                </v-btn>
-            </v-co>
-        </v-row>
-        <v-list item-props lines="one">
-            <v-list-item v-for="getList in getAll" :key="getAll.id" :title="getList.username" :subtitle="getList.email">
-                <router-link id="posts" :to="{ name: 'Posts', params: { id: getList.id } }">
-                    <v-btn variant="tonal">
-                        Ver Perfil
-                    </v-btn>
-                </router-link>
-            </v-list-item>
-        </v-list>
+  <div>
+    <v-row>
+      <v-col>
+        <h1>Users</h1>
+      </v-col>
+      <v-co>
+        <v-btn id="cmd" @click="printPDF" variant="tonal">
+          Imprimir
+        </v-btn>
+      </v-co>
+    </v-row>
+    <div id="content">
+      <v-list item-props lines="one">
+        <v-list-item v-for="getList in getAll" :key="getAll.id" :title="getList.username" :subtitle="getList.email">
+          <router-link id="posts" :to="{ name: 'Posts', params: { id: getList.id } }">
+            <v-btn variant="tonal">
+              Ver Perfil
+            </v-btn>
+          </router-link>
+        </v-list-item>
+      </v-list>
     </div>
+  </div>
 </template>
 
+
 <script>
-import html2pdf from 'html2pdf.js';
 export default {
-    data() {
-        return {
-            getAll: []
-        }
-    },
-    created() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then((response) => response.json())
-            .then((data) => (this.getAll = data))
-    },
-    methods: {
-        convertToPDF() {
-            const element = this.$el; // Captura el elemento que contiene la lista
-            html2pdf().from(element).save();
-        }
+  data() {
+    return {
+      getAll: []
     }
+  },
+  created() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) => (this.getAll = data))
+  },
+  methods: {
+    printPDF() {
+      var doc = new jsPDF();
+      var specialElementHandlers = {
+        '#editor': function (element, renderer) {
+          return true;
+        }
+      };
+      // Agregar un encabezado
+      doc.addImage('https://infimg.com/bimg/2016/11/imagenes-gratis-sin-copyright.jpg', 'JPEG', 15, 15, 50, 50, {
+        'width': 170,
+        'elementHandlers': specialElementHandlers
+      });
+
+      // Agregar un pie de página
+      doc.addImage('https://infimg.com/bimg/2016/11/imagenes-gratis-sin-copyright.jpg', 'JPEG', 15, doc.internal.pageSize.height - 20, 50, 50, {
+        'width': 170,
+        'elementHandlers': specialElementHandlers
+      });
+
+
+      // Agregar un pie de página HTML
+      var footerHtml = `
+    <div style="text-align:center;">
+      <v-row>
+        <v-spacer></v-spacer>
+        <v-col>
+          <v-sheet class="pa-2 ma-2">
+            Footer
+          </v-sheet>
+        </v-col>
+        <v-spacer></v-spacer>
+        <v-col>
+
+        </v-col>
+      </v-row>
+    </div>`;
+      doc.fromHTML(footerHtml, 15, doc.internal.pageSize.height - 15, {
+        'width': 170,
+        'elementHandlers': specialElementHandlers
+      });
+
+      // Agregar un encabezado HTML
+      var headerHtml = `<v-row>
+      <v-col>
+        <v-sheet class="pa-2 ma-2">
+          Header
+        </v-sheet>
+      </v-col>
+      <v-spacer></v-spacer>
+      <v-col>
+      </v-col>
+    </v-row>`;
+      doc.fromHTML(headerHtml, 15, 15, {
+        'width': 170,
+        'elementHandlers': specialElementHandlers
+      });
+
+      doc.fromHTML($('#content').html(), 15, 30, {
+        'width': 170,
+        'elementHandlers': specialElementHandlers
+      });
+      doc.save('users.pdf');
+    }
+  }
 }
+
 </script>
--- src/views/Productos.vue
+++ src/views/Productos.vue
@@ -1,39 +1,62 @@
 <template>
-    <Bar :chart-options="chartOptions" :data="chartData"></Bar>
-    <v-list lines="one">
+  <v-row id="content" align="start" no-gutters>
+    <v-col style="position: sticky; top: 16.0px;" cols="3">
+      <h1>Productos</h1>
+      <div v-if="loading">
+        <center>
+          <v-progress-circular color="info" size="24" indeterminate></v-progress-circular>
+          <h3>Cargando grafica...</h3>
+        </center>
+      </div>
+      <div v-else-if="error">Error: {{ errorMessage }}</div>
+      <div v-else>
+        <!-- Renderizar los datos aquí -->
+        <Bar :chart-options="chartOptions" :data="chartData"></Bar>
+      </div>
+    </v-col>
+    <v-col align="end" cols="9">
+      <div
+        style="position: sticky; top:  16.0px; z-index: 1; background-color: white; padding: 16.0px;box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;">
+        <v-btn @click="printPDF" variant="outlined">
+          Imprimir
+        </v-btn>
+      </div>
+      <v-list lines="one">
         <v-list-item class="column-4" v-for="products in getAllProductos" :key="products.id">
-            <v-card class="mx-auto" max-width="344">
-                <v-img height="200px" :src="products.image" cover></v-img>
-                <v-card-title>
-                    {{ products.title }} - ${{ products.price }}
-                </v-card-title>
+          <v-card class="mx-auto" max-width="200">
+            <v-img height="100px" :src="products.image" cover></v-img>
+            <v-card-title>
+              {{ products.title }} - ${{ products.price }}
+            </v-card-title>
 
-                <v-card-subtitle>
-                    {{ products.category }}
-                </v-card-subtitle>
+            <v-card-subtitle>
+              {{ products.category }}
+            </v-card-subtitle>
 
-                <v-card-actions>
-                    <v-btn color="orange-lighten-2" variant="text">
-                        Explore
-                    </v-btn>
+            <v-card-actions>
+              <v-btn color="orange-lighten-2" variant="text">
+                Explore
+              </v-btn>
 
-                    <v-spacer></v-spacer>
+              <v-spacer></v-spacer>
 
-                    <v-btn :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show = !show"></v-btn>
-                </v-card-actions>
+              <v-btn :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show = !show"></v-btn>
+            </v-card-actions>
 
-                <v-expand-transition>
-                    <div v-show="show">
-                        <v-divider></v-divider>
+            <v-expand-transition>
+              <div v-show="show">
+                <v-divider></v-divider>
 
-                        <v-card-text>
-                            {{ products.description }}
-                        </v-card-text>
-                    </div>
-                </v-expand-transition>
-            </v-card>
+                <v-card-text>
+                  {{ products.description }}
+                </v-card-text>
+              </div>
+            </v-expand-transition>
+          </v-card>
         </v-list-item>
-    </v-list>
+      </v-list>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -43,55 +66,136 @@ import { Bar } from 'vue-chartjs';
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 export default {
-    components: { Bar },
-    props: {
-        chartId: {
-            type: String,
-            default: 'bar-chart'
+  components: { Bar },
+  props: {
+    chartId: {
+      type: String,
+      default: 'bar-chart'
+    }
+  },
+  data() {
+    return {
+      loading: true,
+      error: false,
+      errorMessage: '',
+      getAllProductos: [],
+      show: false,
+      categorias: [],
+      price: [],
+      chartData: [],
+      chartOptions: {
+        responsive: true
+      },
+    }
+  },
+  async mounted() {
+    try {
+      const response = await axios.get('https://fakestoreapi.com/products');
+      const datos = response.data;
+
+      // Simular un retraso para demostrar el estado de carga
+      await new Promise(resolve => setTimeout(resolve, 1000));
+
+      if (Array.isArray(datos) && datos.length > 0) {
+        // Procesar los datos y asignarlos a chartData
+        // En este ejemplo, simplemente mostramos un conjunto de datos vacío
+        this.categorias = [];
+        this.price = [];
+        datos.forEach(row => {
+          if (row.category && row.price) {
+            this.categorias.push(row.category);
+            this.price.push(row.price);
+          }
+        });
+
+        this.chartData = {
+          labels: this.categorias,
+          datasets: [{ label: 'Precios', data: this.price }]
+        };
+      } else {
+        // Mostrar un mensaje de error si no se encontraron datos
+        this.error = true;
+        this.errorMessage = 'No se encontraron datos';
+      }
+    } catch (error) {
+      // Mostrar un mensaje de error si hay un error en la solicitud
+      this.error = true;
+      this.errorMessage = error.message || 'Error al obtener los datos de la API';
+    } finally {
+      // Cambiar el estado de carga después de completar la solicitud
+      this.loading = false;
+    }
+  },
+  created() {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then((data) => (this.getAllProductos = data))
+  },
+  methods: {
+    printPDF() {
+      var doc = new jsPDF();
+      var specialElementHandlers = {
+        '#editor': function (element, renderer) {
+          return true;
         }
-    },
-    data() {
-        return {
-            getAllProductos: [],
-            show: false,
-            categorias: [],
-            price: [],
-            chartData: [],
-            chartOptions: {
-                responsive: true
-            },
-        }
-    },
-    async mounted() {
-        const datos = await axios.get('https://fakestoreapi.com/products');
-        datos.data.map((row) => (
-            this.categorias.push(row.category),
-            this.price.push(row.price)
-        )),
-            this.chartData = {
-                labels: this.categorias,
-                datasets: [
-                    { label: 'Precios', data: this.price }
-                ]
-            }
-    },
-    created() {
-        fetch('https://fakestoreapi.com/products')
-            .then(res => res.json())
-            .then((data) => (this.getAllProductos = data))
-    },
+      };
+
+      // Agregar un pie de página HTML
+      var footerHtml = `
+    <div style="text-align:center;">
+      <v-row>
+        <v-spacer></v-spacer>
+        <v-col>
+          <v-sheet class="pa-2 ma-2">
+            Footer
+          </v-sheet>
+        </v-col>
+        <v-spacer></v-spacer>
+        <v-col>
+
+        </v-col>
+      </v-row>
+    </div>`;
+      doc.fromHTML(footerHtml, 15, doc.internal.pageSize.height - 15, {
+        'width': 170,
+        'elementHandlers': specialElementHandlers
+      });
+
+      // Agregar un encabezado HTML
+      var headerHtml = `<v-row>
+      <v-col>
+        <v-sheet class="pa-2 ma-2">
+          Header
+        </v-sheet>
+      </v-col>
+      <v-spacer></v-spacer>
+      <v-col>
+      </v-col>
+    </v-row>`;
+      doc.fromHTML(headerHtml, 15, 15, {
+        'width': 170,
+        'elementHandlers': specialElementHandlers
+      });
+
+      doc.fromHTML($('#content').html(), 15, 30, {
+        'width': 170,
+        'elementHandlers': specialElementHandlers
+      });
+      doc.save('users.pdf');
+    }
+  }
 }
 </script>
 
 <style scoped>
 .column-4 {
-    width: calc(25% - 16px);
-    /* 25% width for each column with some spacing */
-    margin-right: 16px;
-    /* Adjust spacing between columns as needed */
-    float: left;
-    /* Float the items to create a grid layout */
-    box-sizing: border-box;
-    /* Include padding and border in the element's total width and height */
+  width: calc(25% - 16px);
+  /* 25% width for each column with some spacing */
+  margin-right: 16px;
+  /* Adjust spacing between columns as needed */
+  float: left;
+  /* Float the items to create a grid layout */
+  box-sizing: border-box;
+  /* Include padding and border in the element's total width and height */
 }
 </style>
